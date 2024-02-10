71742029cec4d1030bade9f82c0ec9fadb7e3791
dpaganelli <dpaganelli@binaryvision.com>
2024-02-10 14:13:44 +0000

    updated styles and added tooltip

--- components/webchart/wrapper/index.tsx
+++ components/webchart/wrapper/index.tsx
@@ -15,10 +15,38 @@ const titleCase = (str: string) => {
     .join(" ");
 };
 
+const InfoIcon = () => {
+  return (
+    <svg
+      width="800px"
+      height="800px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="12" cy="12" r="10" stroke="#1C274C" stroke-width="1.5" />
+      <path
+        d="M12 17V11"
+        stroke="#1C274C"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <circle
+        cx="1"
+        cy="1"
+        r="1"
+        transform="matrix(1 0 0 -1 11 9)"
+        fill="#1C274C"
+      />
+    </svg>
+  );
+};
+
 const Wrapper = () => {
   const [data, setData] = useState<IChartType[]>([]);
   const [updatedAt, setUpdatedAt] = useState("");
   const [loading, setLoading] = useState(true);
+  const [showTooltip, setShowTooltip] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,27 +59,43 @@ const Wrapper = () => {
     fetchData();
   }, []);
 
+  const openTooltip = () => {
+    setShowTooltip(true);
+    setTimeout(() => {
+      setShowTooltip(false);
+    }, 5000);
+  };
+
   if (loading) {
     return <div className={style.wrapper}>Loading...</div>;
   }
   return (
     <div className={style.wrapper}>
-      <div className={style.updated_at}>
-        <p className={style.date}>
-          last updated <b>{new Date(updatedAt).toLocaleDateString()}</b>
-        </p>
-        <p className={style.time}>
-          at{" "}
-          {new Date(updatedAt)
-            .toLocaleTimeString()
-            .split(":")
-            .slice(0, 2)
-            .join(":")}
-        </p>
+      <div className={style.tooltip} onClick={openTooltip}>
+        <InfoIcon />
       </div>
+      {showTooltip && (
+        <div className={style.updated_at}>
+          <p className={style.date}>
+            last updated <b>{new Date(updatedAt).toLocaleDateString()}</b>
+          </p>
+          <p className={style.time}>
+            at{" "}
+            {new Date(updatedAt)
+              .toLocaleTimeString()
+              .split(":")
+              .slice(0, 2)
+              .join(":")}
+          </p>
+        </div>
+      )}
       <div className={style.chart}>
         {data.map((d, i) => (
-          <div key={i} className={style.row}>
+          <div
+            key={i}
+            className={style.row}
+            style={{ animationDelay: `${i / 5}s` }}
+          >
             <div className={style.position}>{i + 1}</div>
             <div className={style.artist}>{titleCase(d.artist)}</div>
             <div className={style.song}>{titleCase(d.song)}</div>
